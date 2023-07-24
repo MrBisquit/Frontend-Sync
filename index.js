32e3b47ac6d5789f28aceae66dfae821f22ae35f
MrBisquit <william@wtdawson.info>
2023-07-24 10:19:31 +0100

    .

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/**
+* FrontendSync V1.0.0
+* Easily sync data between frontend and backend seamlessly.
+* Auhtor: MrBisquit (william@wtdawson.info)
+* Licence: MIT
+* Github: https://github.com/MrBisquit/Frontend-Sync
+* NPM: https://www.npmjs.com/package/frontendsync
+*/
+
 const FrontendSync = require("./lib/FrontendSync.js");
 
 // And there was silence...
